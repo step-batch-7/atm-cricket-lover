a4501aaccadb1c94ed9aaf36a3cef5bebe820687
phaneendra <phaneendraandukuri@gmail.com>
2020-04-08 12:00:10 +0530

    implemented get_money function to take amount and return in the form of denominations

---
+++ atm.c
@@ -0,0 +1,17 @@
+#include "atm.h"
+
+unsigned int get_money(unsigned short int amount)
+{
+	unsigned int notes_list = 0x00000000;
+	int num_of_notes = 0;
+	int denominations[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
+
+	for (int index = 0; index < 8; index++)
+	{
+		num_of_notes = amount / denominations[index];
+		amount %= denominations[index];
+		notes_list = num_of_notes | (notes_list << 4);
+	}
+
+	return notes_list;
+}
