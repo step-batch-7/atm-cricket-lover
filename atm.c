1a25ccf2f29a239e646061a8edeaf05f932435be
phaneendra <phaneendraandukuri@gmail.com>
2020-04-08 12:02:38 +0530

    modified get_money function with maximum amount condition

--- atm.c
+++ atm.c
@@ -6,6 +6,11 @@ unsigned int get_money(unsigned short int amount)
 	int num_of_notes = 0;
 	int denominations[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
 
+	if (amount > 31999)
+	{
+		return notes_list;
+	}
+
 	for (int index = 0; index < 8; index++)
 	{
 		num_of_notes = amount / denominations[index];
