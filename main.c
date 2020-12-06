4fb5570afef8e9113c2a231b500cb0e0df3b8402
Phaneendra <phaneendraandukuri@gmail.com>
2020-12-06 12:21:12 +0530

    added main.c

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "atm.h"
 
+void display_notes(unsigned short int money)
+{
+	unsigned int notes_list = 0x00000000;
+	int num_of_notes = 0;
+	int denominations[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
+}
+
 int main(void)
 {
   unsigned short int amounts[] = {1, 10, 39, 589, 2590, 31999};
